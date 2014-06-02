e24f499ba062c6f2346dd48c5bfdfa8d96c445a5
noam787 <zznoam3@gmail.com>
2014-06-02 10:47:12 +0300

    changes made

--- Torrent.py
+++ Torrent.py
@@ -1,1 +1,40 @@
 from flaskServer import *
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+
+class Window(BoxLayout):
+	def __init__(self, title):
+		super(Window,self).__init__()
+
+
+class ConnectionWindowBase(Widget):
+	def __init__(self):
+		super(ConnectionWindowBase,self).__init__()
+	def connect(self):
+		self.parent.parent.remove_widget(self.parent)
+
+class ConnectionWindow(BoxLayout):
+	def __init__(self):
+		super(ConnectionWindow,self).__init__()
+		#self.add_widget(ConnectionWindowBase())
+		#self.add_widget(Window('bla'))
+		
+
+class MainScreen(Widget):
+	def __init__(self):
+		self.connectionWindow = ConnectionWindow()
+		super(MainScreen,self).__init__()
+	def show_connect_window(self):
+		try:
+			self.add_widget(self.connectionWindow)
+		except:
+			pass
+
+class TorrentApp(App):
+	def build(self):
+		window = MainScreen()
+		return window
+
+if __name__=='__main__':
+	TorrentApp().run()
--- parser.py
+++ parser.py
@@ -18,9 +18,13 @@ class Parser(object):
 				self.responseMessage += '000101'
 			else:
 				self.responseMessage += '000100'
-
+			if len(self.dests)>1:
+				self.forwardMessage += '1002' + dat
+			return
+		self.forwardMessage += '1002' + dat
+		
 	def m20(self, dat):
-		ttl = dat[:2]
+		ttl = int(dat[:2],16) - 1
 		sourceAddr = dat[2:10]
 		port = dat[10:14]
 		numOfDest = int(dat[14:16],16)
@@ -29,11 +33,12 @@ class Parser(object):
 			raise ProtocolError()
 		dests={}
 		for i in range(numOfDest):
-			parsedAddr= addrs[i*8:(i+1)*8]
-			parsedPort =addrs[ (numOfDest*8 + i*4) : (numOfDest*8 + (i + 1)*4) ]
+			parsedAddr = addrs[i*8:(i+1)*8]
+			parsedPort = addrs[ (numOfDest*8 + i*4) : (numOfDest*8 + (i + 1)*4) ]
 			dests[parsedAddr] = parsedPort
 		self.sourceAddr = sourceAddr
 		self.dests = dests
+				
 		
 	def m30(self, dat):
 		pass
@@ -45,30 +50,33 @@ class Parser(object):
 	def parseMessage(message):
 		self.dests = {}
 		while len(message)>5:
-			key = message[:2]
-			if not key in ['40']: # 40 massage has 2 byte long length param.
-				data = message[4:(int(message[2:4],16)*2 +4)]
-				if key in ['00' , '10', '20', '30', '41']: # protocol error
-					return {}, '', '000101'
-				if key == '00':
-					m00(data)
-				elif key == '10':
-					m10(data)
-				elif key == '20':
-					if not dests == {}:
-						yield self.dests, self.forwardMessage, self.responseMessage
-  						self.dests = {}
-						self.forwardMessage = ''
-						self.responseMessage = ''
-					m20(data)
-				elif key == '30':
-					m30(data)
-				elif key == '41':
-					m41(data)
-				message = message[(int(message[2:4],16)*2 +4):]
-				continue
-			data = message[6:(int(message[2:6],16)*2 +6)]
-			m40(data)
-			message = message[(int(message[2:6],16)*2 +6):]
-			yield self.dests, self.forwardMessage, self.responseMessage  
+			try:
+				key = message[:2]
+				if not key in ['40']: # 40 massage has 2 byte long length param.
+					data = message[4:(int(message[2:4],16)*2 +4)]
+					if key in ['00' , '10', '20', '30', '41']: # protocol error
+						raise ProtocolError()
+					if key == '00':
+						m00(data)
+					elif key == '10':
+						m10(data)
+					elif key == '20':
+						if not dests == {}:
+							yield self.dests, self.forwardMessage, self.responseMessage
+	  						self.dests = {}
+							self.forwardMessage = ''
+							self.responseMessage = ''
+						m20(data)
+					elif key == '30':
+						m30(data)
+					elif key == '41':
+						m41(data)
+					message = message[(int(message[2:4],16)*2 +4):]
+					continue
+				data = message[6:(int(message[2:6],16)*2 +6)]
+				m40(data)
+				message = message[(int(message[2:6],16)*2 +6):]
+				yield self.dests, self.forwardMessage, self.responseMessage  
+			except:
+				yield {}, '', '000101'
 
